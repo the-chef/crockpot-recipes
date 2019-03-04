234eafcf251fa197c329790ac2ff23687778f436
the-chef <18197047+the-chef@users.noreply.github.com>
2019-03-04 17:23:42 -0600

    Add files via upload

--- test538.ps1
+++ test538.ps1
@@ -9,23 +9,23 @@ $global:n = 1
 
 do {
 	$game = $odds | Where-Object {$_.gamenum -eq $global:n}
-	if ($game[0].538 -gt $confidence -and $game[0].final -gt $game[1].final){
+	if ($game[0].538 -gt $confidence -and [int]$game[0].final -gt [int]$game[1].final){
 		$global:winnings = (-1 * $wager / ($game.open[0] / 100)) + $global:winnings
-		write-host 538 had $game[0].538 confidence that $game[0].team $game[0].final would beat $game[1].team $game[1].final Winnings: $global:winnings
+		write-host 1 538 had $game[0].538 confidence that $game[0].team $game[0].final would beat $game[1].team $game[1].final Winnings: $global:winnings
 		$global:correct++
 																			}																		
-	elseif ($game[1].538 -gt $confidence -and $game[1].final -gt $game[0].final){
+	elseif ($game[1].538 -gt $confidence -and [int]$game[1].final -gt [int]$game[0].final){
 		$global:winnings = (-1 * $wager / ($game.open[1] / 100)) + $global:winnings
-		write-host 538 had $game[1].538 confidence that $game[1].team $game[1].final would beat $game[0].team $game[0].final Winnings: $global:winnings
+		write-host 2 538 had $game[1].538 confidence that $game[1].team $game[1].final would beat $game[0].team $game[0].final Winnings: $global:winnings
 		$global:correct++
 																			}
-	elseif 	($game[0].538 -gt $confidence -and $game[0].final -lt $game[1].final) {
+	elseif 	($game[0].538 -gt $confidence -and [int]$game[0].final -lt [int]$game[1].final) {
 			$global:losses = ($global:losses-$wager)
-			write-host You Lose! 538 had $game[0].538 confidence that $game[0].team $game[0].final would beat $game[1].team $game[1].final Losses: $global:losses
+			write-host 3 You Lose! 538 had $game[0].538 confidence that $game[0].team $game[0].final would beat $game[1].team $game[1].final Losses: $global:losses
 			}
-	elseif 	($game[1].538 -gt $confidence -and $game[1].final -lt $game[0].final) {
+	elseif 	($game[1].538 -gt $confidence -and [int]$game[1].final -lt [int]$game[0].final) {
 			$global:losses = ($global:losses-$wager)
-			write-host You Lose! 538 had $game[1].538 confidence that $game[1].team $game[1].final would beat $game[0].team $game[0].final Losses: $global:losses
+			write-host 4 You Lose! 538 had $game[1].538 confidence that $game[1].team $game[1].final would beat $game[0].team $game[0].final Losses: $global:losses
 			}
 $global:n++
 }

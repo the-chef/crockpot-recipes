f2ae847273ad809d3c681828cb3e29b8a8587927
the-chef <18197047+the-chef@users.noreply.github.com>
2019-03-06 12:17:55 -0600

    Add files via upload

---
+++ CheckEm.ps1
@@ -0,0 +1,57 @@
+param($confidence = .6, $wager = 10, $year = 2018, $team = "NA", $startdate = "NA", $stopdate = "NA")
+$odds = Import-Csv c:\opat\2018.csv
+$global:correct = 0
+$global:incorrect = 0
+$global:winnings = 0
+$global:losses = 0
+
+if ($team -eq "NA") {}
+else {
+	$odds = ($odds | Where-Object {$_.home -eq $team -or $_.visitor -eq $team})
+	}
+
+if ($startdate -eq "NA") {}
+else {
+	$odds = ($odds | Where-Object {$_.date -gt $startdate})
+	}
+
+if ($stopdate -eq "NA") {}
+else {
+	$odds = ($odds | Where-Object {$_.date -lt $stopdate})
+	}
+
+foreach ($game in $odds) {
+	#write-host $game.home $game.finalh $game.h538 vs $game.visitor $game.finalv $game.v538
+	if ([int]$game.finalh -gt [int]$game.finalv -and $game.H538 -gt $confidence) {
+		write-host $game.date 538 correctly chose the home team $game.finalh $game.home $game.H538 to beat $game.finalv $game.visitor $game.V538 on $game.date and the money line was $game.openh
+		$payout = (-1 * [int]$wager / ([int]$game.openh/100))
+		if ($payout -lt 0) {$payout = ($payout * -1)}
+		$global:winnings = $payout + $global:winnings
+		write-host Winnings: $global:winnings
+		$global:correct++
+		}
+	elseif ([int]$game.finalv -gt [int]$game.finalh -and $game.V538 -gt $confidence) {
+		write-host $game.date 538 correctly chose the visiting team $game.finalv $game.visitor $game.V538 to beat $game.finalh $game.home $game.H538 on $game.date and the money line was $game.openv
+		$payout = (-1 * [int]$wager / ([int]$game.openh/100))
+		if ($payout -lt 0) {$payout = ($payout * -1)}
+		$global:winnings = $payout + $global:winnings
+		write-host Winnings: $global:winnings
+		$global:correct++
+		}
+	elseif ($game.v538 -lt $confidence -and $game.h538 -lt $confidence){}
+	else  {
+		write-host $game.date 538 incorrectly chose the winner of $game.home $game.finalh $game.h538 vs $game.visitor $game.finalv $game.v538
+		$global:losses = ($global:losses-$wager)
+		write-host Losses: $global:losses
+		$global:incorrect++
+		}
+					}
+					
+$total = ($correct + $incorrect)
+Write-host $total games played
+Write-host $correct guessed correctly
+Write-host $incorrect guessed incorrectly
+$percentage = ($correct / $total)
+Write-host Confidence of $confidence.tostring("P") resulted in correct predictions $percentage.tostring("P") of the time
+$themoney = ($winnings + $losses)
+Write-host With a static wager of $wager the return is $themoney

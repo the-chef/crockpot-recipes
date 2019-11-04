30e35b44b1c561e8e5490fa977108ab6430c1b3e
the-chef <18197047+the-chef@users.noreply.github.com>
2019-11-04 12:36:38 -0600

    Update aws_prod.sh

--- aws_prod.sh
+++ aws_prod.sh
@@ -2,5 +2,5 @@
 subscription-manager clean
 yum clean all
 rm -f /etc/yum.repos.d/*.repo
-/bin/cp -f /Linux_Install_CDs/hardening/rhel7-Prod.repo /etc/yum.repos.d/
+wget -O /etc/yum.repos.d/rhel7-Prod.repo https://raw.githubusercontent.com/the-chef/crockpot-recipes/master/rhel7-Prod.repo
 yum check-update

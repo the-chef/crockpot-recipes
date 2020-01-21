141f26f00ad65d41fb0e480def48507ce2578f1f
the-chef <18197047+the-chef@users.noreply.github.com>
2020-01-21 10:57:47 -0600

    Update NessusAgentInstall_RHEL7.sh

--- NessusAgentInstall_RHEL7.sh
+++ NessusAgentInstall_RHEL7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/the-chef/crockpot-recipes/raw/master/NessusAgent-7.5.1-es7.x86_64.rpm
+curl -OJ "https://github.com/the-chef/crockpot-recipes/raw/master/NessusAgent-7.5.1-es7.x86_64.rpm"
 rpm -Uvh NessusAgent*.rpm
 /opt/nessus_agent/sbin/nessuscli agent link --key=c96e52d76984b1fefe7183284ca57d0b31bcd0b04ce50f244c78196389e28a67 --groups="AWS, Linux, RHEL7" --host=cloud.tenable.com --port=443
 systemctl start nessusagent.service
